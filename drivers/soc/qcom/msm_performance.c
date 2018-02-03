98b2a0911b5b8918720bff533312d2742262ffd1
Nathan Chancellor <natechancellor@gmail.com>
2018-02-02 23:23:24 -0700

    soc: qcom: msm_perf: Kill max freq handling too
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -122,6 +122,7 @@ module_param_cb(cpu_min_freq, &param_ops_cpu_min_freq, NULL, 0644);
 
 static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 {
+#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -166,6 +167,7 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
+#endif
 
 	return 0;
 }
