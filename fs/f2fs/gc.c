d8b8a69e097cafbda6107883c3307fc41f271e39
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-01 21:55:33 +0900

    f2fs: explicitly flush device upon foreground GC
    GC itself will produce some dirty segments.
    This will allow more GCs to be made upon rapid GC or urgent GC.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1404,24 +1404,25 @@ int f2fs_gc(struct f2fs_sb_info *sbi, bool sync,
 		.ilist = LIST_HEAD_INIT(gc_list.ilist),
 		.iroot = RADIX_TREE_INIT(GFP_NOFS),
 	};
+	struct super_block *sb = sbi->sb;
 	unsigned long long last_skipped = sbi->skipped_atomic_files[FG_GC];
 	unsigned long long first_skipped;
 	unsigned int skipped_round = 0, round = 0;
 
-	trace_f2fs_gc_begin(sbi->sb, sync, background,
-				get_pages(sbi, F2FS_DIRTY_NODES),
-				get_pages(sbi, F2FS_DIRTY_DENTS),
-				get_pages(sbi, F2FS_DIRTY_IMETA),
-				free_sections(sbi),
-				free_segments(sbi),
-				reserved_segments(sbi),
-				prefree_segments(sbi));
+	//trace_f2fs_gc_begin(sb, sync, background,
+	//			get_pages(sbi, F2FS_DIRTY_NODES),
+	//			get_pages(sbi, F2FS_DIRTY_DENTS),
+	//			get_pages(sbi, F2FS_DIRTY_IMETA),
+	//			free_sections(sbi),
+	//			free_segments(sbi),
+	//			reserved_segments(sbi),
+	//			prefree_segments(sbi));
 
 	cpc.reason = __get_cp_reason(sbi);
 	sbi->skipped_gc_rwsem = 0;
 	first_skipped = last_skipped;
 gc_more:
-	if (unlikely(!(sbi->sb->s_flags & MS_ACTIVE))) {
+	if (unlikely(!(sb->s_flags & MS_ACTIVE))) {
 		ret = -EINVAL;
 		goto stop;
 	}
@@ -1496,6 +1497,7 @@ stop:
 	SIT_I(sbi)->last_victim[ALLOC_NEXT] = 0;
 	SIT_I(sbi)->last_victim[FLUSH_DEVICE] = init_segno;
 
+<<<<<<< HEAD
 	trace_f2fs_gc_end(sbi->sb, ret, total_freed, sec_freed,
 				get_pages(sbi, F2FS_DIRTY_NODES),
 				get_pages(sbi, F2FS_DIRTY_DENTS),
@@ -1504,6 +1506,16 @@ stop:
 				free_segments(sbi),
 				reserved_segments(sbi),
 				prefree_segments(sbi));
+=======
+	//trace_f2fs_gc_end(sb, ret, total_freed, sec_freed,
+	//			get_pages(sbi, F2FS_DIRTY_NODES),
+	//			get_pages(sbi, F2FS_DIRTY_DENTS),
+	//			get_pages(sbi, F2FS_DIRTY_IMETA),
+	//			free_sections(sbi),
+	//			free_segments(sbi),
+	//			reserved_segments(sbi),
+	//			prefree_segments(sbi));
+>>>>>>> 69fe5f15713d... f2fs: explicitly flush device upon foreground GC
 
 	mutex_unlock(&sbi->gc_mutex);
 
@@ -1511,6 +1523,13 @@ stop:
 
 	if (sync && !ret)
 		ret = sec_freed ? 0 : -EAGAIN;
+
+	if (gc_type == FG_GC && down_read_trylock(&sb->s_umount)) {
+		writeback_inodes_sb(sb, WB_REASON_SYNC);
+		sync_inodes_sb(sb);
+		up_read(&sb->s_umount);
+	}
+
 	return ret;
 }
 
